8ef9135a8b3cf8f71e12c240f7971274ecbff41f
jailson silva <jailsonnogueira2@gmail.com>
2022-05-10 01:55:35 -0300

    corrigindo o erro de import

--- src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -1,18 +1,23 @@
 package com.dio.santander.bankline.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dio.santander.bankline.api.model.Correntista;
-
-import antlr.collections.List;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
 
 @RestController
-@RequestMapping("/correntistas")
+@RequestMapping(" /correntistas")
 public class CorrentistaController {
-		public List<Correntista> findAll(){
-			
-		}
-
-}
+	@Autowired
+	private CorrentistaRepository repository;
 	
+	@GetMapping
+	public List<Correntista> findAll(){
+		return repository.findAll();
+	}
+}
