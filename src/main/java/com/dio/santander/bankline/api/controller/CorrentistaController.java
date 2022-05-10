5bbc964750a813a34af5fafbc56f13d40a3ba833
jailson silva <jailsonnogueira2@gmail.com>
2022-05-10 00:17:22 -0300

    Erro no pacote de controller volto quando trm

---
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -0,0 +1,18 @@
+package com.dio.santander.bankline.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dio.santander.bankline.api.model.Correntista;
+
+import antlr.collections.List;
+
+@RestController
+@RequestMapping("/correntistas")
+public class CorrentistaController {
+		public List<Correntista> findAll(){
+			
+		}
+
+}
+	
--- src/main/java/com/dio/santander/banklineapi/controler/CorrentistaControler.java
+++
@@ -1,7 +0,0 @@
-package com.dio.santander.banklineapi.controler;
-
-public class CorrentistaControler {
-	
-	// Apartir de agora a nossa aplicação precisa ser web
-
-}
